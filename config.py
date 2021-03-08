ecc9205c7470711b9d284a0a7c3471aad426b141
kadas36 <oduorthomas14@gmail.com>
2021-03-08 16:51:01 +0300

    req.txt

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    # SQLALCHEMY_DATABASE_URI="postgres://moringa:Access@localhost:5432/amos"
+    SQLALCHEMY_DATABASE_URI="postgres://moringa:Access@localhost:5432/amos"
     print (SQLALCHEMY_DATABASE_URI, "00000")
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
