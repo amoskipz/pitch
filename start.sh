843579d5e98ae4fc19a0d3592a80fe063c948c34
kadas36 <oduorthomas14@gmail.com>
2021-03-08 12:53:44 +0300

    pitch

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-# export MAIL_USERNAME=koechkipz03@@gmail.com
-# export MAIL_PASSWORD=amos3378
+export MAIL_USERNAME=koechkipz03@@gmail.com
+export MAIL_PASSWORD=amos3378
 # export SECRET_KEY=0000
-# export DATABASE_URL="postgres://moringa:Access@localhost:5432/amos"
-# python3.6 manage.py server
+SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+export DATABASE_URL="postgres://moringa:Access@localhost:5432/amos"
+python3.6 manage.py server
